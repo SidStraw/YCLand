cf21c99bdacc78de366777e26dc2b07273d0197e
Sid-Straw <j80304xz@gmail.com>
2020-01-06 02:43:15 +0800

    feat: 聯絡我們

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,64 @@
 <template>
   <div id="contact">
-    <BlockTitle title="專業團隊" />
+    <BlockTitle title="聯絡我們" />
     <v-container>
-      <v-layout
-        v-for="(item, index) in team"
-        :key="item.title"
-        :class="index % 2 ? '' : 'grey lighten-3'"
+      <h2
+        :class="[
+          $vuetify.breakpoint.xsOnly ? 'display-1' : 'display-2',
+          'font-weight-bold',
+          'text-center',
+          'mt-9'
+        ]"
       >
-        <div style="flex: 0 0 10rem" class="text-center">{{ item.title }}</div>
-        <v-flex v-html="item.value"></v-flex>
-      </v-layout>
+        免費諮詢現勘 歡迎與我們聯繫
+      </h2>
+      <h3
+        :class="[
+          $vuetify.breakpoint.xsOnly ? 'headline' : 'display-1',
+          'font-weight-medium',
+          'text-center',
+          'mt-1'
+        ]"
+      >
+        關懷在地產業 輔導合法經營
+      </h3>
+      <div class="mt-9 mx-auto text-center" style="min-width:50px">
+        <p>
+          <v-icon>mdi-home-map-marker</v-icon> 臺中市西區台灣大道二段536號6樓-1
+        </p>
+        <p>
+          <a href="tel:+886-4-23121667" class="link" target="_blank">
+            <v-icon>mdi-phone-classic</v-icon> 04-23121667
+          </a>
+
+          <a href="tel:+886-4-23133016" class="link" target="_blank">
+            <v-icon>mdi-fax</v-icon> 04-23133016
+          </a>
+        </p>
+        <p>
+          <a href="tel:+886-980-627968" class="link" target="_blank">
+            <v-icon>mdi-cellphone-iphone</v-icon> 0980-627968
+          </a>
+        </p>
+        <p>
+          <a href="mailto:youcheng108@gmail.com" class="link" target="_blank">
+            <v-icon>mdi-email-multiple-outline</v-icon>
+            youcheng108@gmail.com
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://www.facebook.com/ycland/"
+            class="link"
+            target="_blank"
+          >
+            <v-icon>mdi-facebook-box</v-icon> 佑埕粉絲團
+          </a>
+        </p>
+      </div>
+      <!-- <v-layout>
+        <v-flex></v-flex>
+      </v-layout> -->
     </v-container>
   </div>
 </template>
@@ -18,49 +67,19 @@
 import BlockTitle from "@/components/BlockTitle";
 
 export default {
-  name: "Team",
+  name: "Contact",
 
   components: {
     BlockTitle
   },
 
-  data: () => ({
-    team: [
-      {
-        title: "建築師姓名",
-        value: "王銘山"
-      },
-      {
-        title: "學　歷",
-        value: "逢甲大學建築及都市計畫研究所碩士"
-      },
-      {
-        title: "現　任",
-        value: `宇城建築師事務所  主持建築師<br>國立勤益技術學院  景觀設計與管理系兼任講師`
-      },
-      {
-        title: "經　歷",
-        value: `國科會中部科學工業園區籌備處  組員<br>交通部觀光局日月潭國家風景區管理處  專員、課長<br>交通部觀光局東北角海岸國家風景區管理處  技士<br>台中市政府交通局  課長`
-      },
-      {
-        title: "考試及資格",
-        value: `開業建築師 / 專門職業及技術人員高等考試建築師考試及格 / 公務人員高等考試土木工程科系建築工程科考試及格 / 土地登記專業代理人 / 特種考試土地登記專業代理人考試及格`
-      },
-      {
-        title: "主要專長",
-        value: `建築規劃設計 / 景觀工程設計 / 不動產登記事務`
-      },
-      {
-        title: "參與案例",
-        value: `(1)中部科學工業園區管理中心專案管理暨新建統包工程<br>(2)中部科學工業園區建築執照審查及都市設計審議<br>(3)龜山島入口意象暨景觀工程計畫<br>(5)日月潭國家風景區車埕木業博物館新建工程<br>(6)日月潭國家風景區水社遊客服務中心興建工程<br>(7)台中市大坑風景區六號步道整建工程<br>(8)台中市旱溪自行車道一、二、三期規劃設計、監造<br>(9)台中市大墩路店舖、辦公室新建工程<br>(10)台中市潭子區住宅新建工程<br>(11)台中市北區太平路店舖、辦公室、住宅新建工程<br>(12)台中市西區大全街住宅新建工程<br>(13)中部科學工業園區豐聯資訊辦公室-室內裝修規劃設計、施工<br>(14)中部科學工業園區寶麗明廠房-室內裝修規劃設計、施工<br>(15)中部科學工業園區大青節能科技廠房-室內裝修規劃設計、施工<br>(16)台中市西屯區青海南街視聽歌唱場所新建工程<br>(17)台中市西屯區享溫馨KTV台中店新建工程<br>(18)台中市西屯區安和路住宅新建工程<br>(19)台中市豐原區豐社段店舖、住宅新建工程<br>(20)台中市西屯區西苑二街住宅新建工程`
-      }
-    ]
-  })
+  data: () => ({})
 };
 </script>
 
 <style lang="scss" scoped>
-.logo-img {
-  flex: 1 1 300px;
+.link {
+  color: #424242;
+  text-decoration: none;
 }
 </style>
